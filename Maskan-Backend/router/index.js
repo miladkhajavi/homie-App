010603c688a65fe8426b6c39290eed6e3185fadd
milad <mkhajavi5@gmail.com>
2021-02-15 00:02:43 +0330

    add swagger path to index route

--- Maskan-Backend/router/index.js
+++ Maskan-Backend/router/index.js
@@ -3,8 +3,13 @@ import userRouter from "./user";
 import estateRouter from './estate'
 import realEstateRouter from './realEstate'
 import reserveRouter from './reserve'
+import swagger from "swagger-ui-express"
+import swaggerDoc from "../documents/swagger.json"
 
 const router = express.Router();
+
+router.use('/homie/api-ui', swagger.serve);
+router.get('/homie/api-ui',swagger.setup(swaggerDoc))
 
 router.use('/user' , userRouter)
 router.use('/reserve', reserveRouter)
