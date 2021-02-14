c758ca62f31152668ef3a5e721d86a544d0a5d73
milad <mkhajavi5@gmail.com>
2021-02-15 00:00:38 +0330

    move documents and remove some files

--- Maskan-Backend/index.js
+++ Maskan-Backend/index.js
@@ -6,9 +6,9 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import Router from "./router/index";
-import {ConnectDataBase} from "./config/dbConfig";
-import swagger from "swagger-ui-express"
-import swaggerDoc from "./swagger.json"
+import {
+  ConnectDataBase
+} from "./config/dbConfig";
 import path from 'path';
 
 import winston from './config/winston'
@@ -37,7 +37,7 @@ realTime(io)
 // db config
 ConnectDataBase()
 
-app.use('/homie/api-ui',swagger.serve,swagger.setup(swaggerDoc))
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -51,7 +51,9 @@ app.use('/', express.static(path.join(__dirname, 'upload')))
 
 app.use(express.static(path.join(__dirname, 'dist')));;
 
-app.use(morgan("combined",{stream:winston.stream}));
+app.use(morgan("combined", {
+  stream: winston.stream
+}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -65,9 +67,9 @@ app.use("/", Router);
 
 
 
-app.use((err,req, res, next) => {
+app.use((err, req, res, next) => {
   // res.render('/dist/404.html')
-  res.sendFile(path.join(__dirname+'/dist/404.html'));
+  res.sendFile(path.join(__dirname + '/dist/404.html'));
 
   // add this line to include winston logging
   winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
